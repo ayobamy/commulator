78fa7f51322a14d4b84a64089453a6cdebb0e5cf
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-11-20 20:52:18 +0000

    [update]: form functionality

--- web_static/scripts/form.js
+++ web_static/scripts/form.js
@@ -2,5 +2,5 @@ const container = document.querySelector(".container")
 const signUpBtn = document.querySelector(".bkg button")
 
 signUpBtn.addEventListener("click", () => {
-    
+    container.classList.toggle("change");
 });
