ca9291fd67abf3a8111ae11f6091422c36801955
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-12-26 00:14:59 +0000

    [updated]: deleted files

--- web_static/scripts/form.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector('.container');
-const signup = document.querySelector('.bkg button');
-
-signup.addEventListener("click", ()=>{
-    container.classList.toggle("change");
-});
